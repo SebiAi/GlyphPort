eb5936a34632387dd17ca70bb4503ead1b295664
SebiAi <41116921+SebiAi@users.noreply.github.com>
2025-09-16 02:58:35 +0200

    Add utils for phone detection

---
+++ app/src/main/java/com/sebiai/glyphport/Utils.kt
@@ -0,0 +1,36 @@
+package com.sebiai.glyphport
+
+import android.os.Build
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+val screenPaddingModifier = Modifier.padding(12.dp)
+
+enum class PhoneModel(val phoneName: String, val build: String, val supportedZones: Array<UInt>) {
+    PHONE1("Phone (1)", "Spacewar", arrayOf(5u, 15u)),
+    PHONE2("Phone (2)", "Pong", arrayOf(33u)),
+    PHONE2A("Phone (2a)", "Pacman", arrayOf(26u)),
+    PHONE2A_PLUS("Phone (2a) Plus", "PacmanPro", arrayOf(26u)),
+    // Only together because there is no difference in the metadata
+    PHONE3A_AND_PRO("Phone (3a) / Phone (3a) Pro", "Asteroids", arrayOf(36u)),
+    PHONE3("Phone (3)", "Metroid", arrayOf(625u)),
+}
+
+/**
+ * Get the current nothing phone model the app is running on.
+ *
+ * @return The phone model or null if the device is not recognized as
+ * a Nothing phone.
+ */
+fun currentPhoneModel(): PhoneModel? {
+    return when (Build.MODEL) {
+        "A063" -> PhoneModel.PHONE1
+        "A065", "AIN065" -> PhoneModel.PHONE2
+        "A142" -> PhoneModel.PHONE2A
+        "A142P" -> PhoneModel.PHONE2A_PLUS
+        "A059", "A059P" -> PhoneModel.PHONE3A_AND_PRO
+        "A024" -> PhoneModel.PHONE3
+        else -> null
+    }
+}
