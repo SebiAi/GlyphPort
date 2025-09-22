d5f9e4ec268e1797a243b375c80b4cd09f5b4b91
SebiAi <41116921+SebiAi@users.noreply.github.com>
2025-09-22 21:40:09 +0200

    Implement `LightDataTransformerRegistry` - initializes transformers and their groups and makes them searchable

---
+++ app/src/main/java/com/sebiai/glyphport/dataclasses/LightDataTransformerRegistry.kt
@@ -0,0 +1,38 @@
+package com.sebiai.glyphport.dataclasses
+
+import com.sebiai.glyphport.PhoneModel
+import com.sebiai.glyphport.dataclasses.transformer.DefaultPhone1ToPhone2LightDataTransformer
+
+// Interface would be required here if we want to make it testable.
+object LightDataTransformerRegistry {
+    /**
+     * First element in the pair is the input phone model, second one the output phone model of the
+     * transformer group.
+     */
+    private val collections by lazy { // Thread safe initialization
+        createTransformerCollections().associateBy { it.handles to it.outputs }
+    }
+
+    fun getSupportedConversions(input: PhoneModel): Set<PhoneModel> {
+        return collections.keys
+            .filter { it.first == input }
+            .map { it.second }
+            .toSet()
+    }
+
+    fun getCollection(input: PhoneModel, output: PhoneModel): LightDataTransformerCollection? {
+        return collections[input to output]
+    }
+
+    private fun createTransformerCollections(): List<LightDataTransformerCollection> {
+        return listOf(
+            LightDataTransformerCollection(
+                handles = PhoneModel.PHONE1,
+                outputs = PhoneModel.PHONE2,
+                transformers = listOf(
+                    DefaultPhone1ToPhone2LightDataTransformer()
+                )
+            )
+        )
+    }
+}
