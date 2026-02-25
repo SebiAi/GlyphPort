ca78832f44854346a6eca8de1609bcd5b7a8bcdb
SebiAi <41116921+SebiAi@users.noreply.github.com>
2026-02-25 23:39:07 +0100

    Prepend version name with `v`

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         targetSdk = 36
         // Don't forget to change on version update!
         versionCode = 1
-        versionName = "1.0.0"
+        versionName = "v1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
