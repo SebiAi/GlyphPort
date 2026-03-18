15a88a3de06236e953da6ca3a8ae67739b2e97df
SebiAi <41116921+SebiAi@users.noreply.github.com>
2026-03-18 23:06:40 +0100

    Bump version to `v2.0.2`

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         minSdk = 31
         targetSdk = 36
         // Don't forget to change on version update!
-        versionCode = 4
-        versionName = "2.0.1"
+        versionCode = 5
+        versionName = "2.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
